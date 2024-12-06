c71d1e1029122036272aef48215c1fec7960fa8e
KlavitoOfTech <ejibeclaver0@gmail.com>
2024-12-06 07:55:06 -0800

    added some commits to the apptest file

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,20 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// // Challenge: Map over the things Array to generate a <p> element for each item and render them on the page below the button
+
+// function App() {
+//   const thingsArray = ["Thing 1", "Thing 2"]
+//   const thingsElements = thingsArray.map(thing, index => <p key={index}>{thing}</p>
+//   );
+  
+//   return (
+//     <div>
+//       <button>Add Item</button>
+//       {thingsElements}
+//     </div>
+  
+//   )
+// }
+
