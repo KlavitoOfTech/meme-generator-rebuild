1681396be01860cdb1e187292438204418947e86
KlavitoOfTech <ejibeclaver0@gmail.com>
2025-02-09 11:24:57 +0100

    added a choose file button to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
-import React from "react"
+import trollFace from "../images/troll-face.png";
 
 export default function Header() {
     return (
-        <header className="header">
+            <header className="header">
             <img 
-                src="../images/troll-face.png" 
-                className="header--image"
+                src={trollFace} 
+                className="header--image" 
+                alt="Troll Face"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <button className="header--project">Choose File</button>
         </header>
-    )
+    );
 }
